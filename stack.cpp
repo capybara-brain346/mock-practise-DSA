21f7db6841d1fa3d56c6c4560a736ffd9de5de50
Piyush Choudhari <choudhari.piyush@gmail.com>
2023-12-07 17:26:06 +0530

    stack practise

--- stack.cpp
+++ stack.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+#include <stdio.h>
+#define SIZE 10
 using namespace std;
-#define MAX_SIZE 10
 
 class Stack{
     int top;
-    char stack[MAX_SIZE];
+    char stack[SIZE];
 
     public:
-    Stack() : top(-1) {}
-    void push(char);
+    Stack(): top(-1) {}
+    void push(char x);
     char pop();
     bool isFull();
     bool isEmpty();
 };
 
 void Stack::push(char x){
-    if (top < MAX_SIZE-1){
-        stack[++top] = x;
-    } else{
+    if (top<SIZE-1){
+        stack[++top]=x;
+    }else{
         cout << "Stack Overflow!" << "\n";
     }
 }
@@ -25,15 +26,14 @@ void Stack::push(char x){
 char Stack::pop(){
     if (top>=0){
         return stack[top--];
-    } else {
+    } else{
         cout << "Stack Underflow!" << "\n";
-        return '\0';
     }
 }
 
 bool Stack::isFull(){
-    return top == MAX_SIZE-1;
-}
+    return top==SIZE-1;
+} 
 
 bool Stack::isEmpty(){
     return top == -1;
@@ -41,33 +41,30 @@ bool Stack::isEmpty(){
 
 int main(){
     Stack s;
-    char exp[20], ch;
-    int i = 0;
+    char exp[20],ch;
+    int i=0;
 
     cin >> exp;
 
-    if (exp[0] == ')' || exp[0] == ']' || exp[0] == '}'){
-        cout << "Invalid Expression!" << "\n";
-        return 0;
+    if (exp[0]==')' || exp[0]=='}' || exp[0]==']'){
+        printf("Invalid Expression!");
     } else{
-        while (exp[i]!='\0') {
+        while (exp[i]!='\0'){
             ch = exp[i];
-            switch (ch)
-            {
-            case '(': s.push(ch); break;
-            case '{': s.push(ch); break;
-            case '[': s.push(ch); break;
-            case ')': s.pop(); break;
-            case '}': s.pop(); break;
-            case ']': s.pop(); break;
+            switch(ch){
+                case '(': s.push(ch); break;
+                case '{': s.push(ch); break;
+                case '[': s.push(ch);break;
+                case ')': s.pop();break;
+                case '}': s.pop(); break;
+                case ']': s.pop();break;
             }
-            i = i+1;
+            i++;
         }
+        
     }
     if (s.isEmpty()){
-        cout << "Expression is well parentheised";
-    } else{
-        cout << "Invalid!";
-    }
-}
+        printf("Valid!");
+    } else{printf("Invalid Expression!");}
 
+}
