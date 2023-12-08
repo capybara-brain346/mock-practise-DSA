ea16300c3bb6cb41828db5ae99d9782e25aeafed
Piyush Choudhari <choudhari.piyush@gmail.com>
2023-12-08 14:25:13 +0530

    Refactored with vector container

--- OOP/Exp6.cpp
+++ OOP/Exp6.cpp
@@ -1,68 +1,60 @@
-/*Exp 6:Write C++ program using STL for sorting and searching user defined records such as personal records (Name, DOB, Telephone number etc) using vector container.*/
 #include <iostream>
+#include <vector>
 #include <algorithm>
-using namespace std;
+#include <string>
 
-class person
-{
-    public:
-    string name;
-    string dob;
-    long tel;
+struct PersonalRecord {
+    std::string name;
+    std::string dob;
+    std::string phoneNumber;
 
-    void getdata()
-    {
-        cout<<"ENTER NAME: "<<endl;
-        cin>>name;
-        cout<<"ENTER BIRTHDATE: "<<endl;
-        cin>>dob;
-        cout<<"ENTER PHONE NUMBER: "<<endl;
-        cin>>tel;
-    }
-    void displaydata()
-    {
-        cout<<"\nNAME :-"<<name<<endl;
-        cout<<"DATE OF BIRTH:-"<<dob<<endl;
-        cout<<"TELEPHONE :-"<<tel<<endl;
-    }
+    // Constructor for easy initialization
+    PersonalRecord(const std::string& n, const std::string& d, const std::string& phone)
+        : name(n), dob(d), phoneNumber(phone) {}
 };
 
-bool compare(const person & a, const person & b)
-{
-    return a.name < b.name;
+// Function to display a personal record
+void displayRecord(const PersonalRecord& record) {
+    std::cout << "Name: " << record.name << "\tDOB: " << record.dob << "\tPhone: " << record.phoneNumber << std::endl;
 }
 
-int main()
-{
-    int n,c;
-    cout<<"ENTER TOTAL NUMBER OF RECORDS :-";
-    cin>>n;
-    string z[n];
-    person p[n];
-    for(int i=0;i<n;i++)
-    {
-        p[i].getdata();
+// Function to compare two records by name for sorting
+bool compareByName(const PersonalRecord& record1, const PersonalRecord& record2) {
+    return record1.name < record2.name;
+}
+
+int main() {
+    std::vector<PersonalRecord> records;
+
+    // Adding some sample records
+    records.push_back({"John Doe", "1990-05-15", "123-456-7890"});
+    records.push_back({"Alice Smith", "1985-08-22", "987-654-3210"});
+    records.push_back({"Bob Johnson", "1978-12-10", "555-123-4567"});
+
+    // Sorting records by name
+    std::sort(records.begin(), records.end(), compareByName);
+
+    // Displaying sorted records
+    std::cout << "Sorted Records:\n";
+    for (const auto& record : records) {
+        displayRecord(record);
     }
-   
-    cout<<"*****SORTED RECORD******"<<endl;
-    sort(p, p+n, compare);
-    for(int i=0;i<n;i++)
-    {
-        p[i].displaydata();
+
+    // Searching for a record by name
+    std::string searchName;
+    std::cout << "\nEnter a name to search: ";
+    std::getline(std::cin, searchName);
+
+    auto it = std::find_if(records.begin(), records.end(), [searchName](const PersonalRecord& record) {
+        return record.name == searchName;
+    });
+
+    if (it != records.end()) {
+        std::cout << "Record found:\n";
+        displayRecord(*it);
+    } else {
+        std::cout << "Record not found.\n";
     }
-    cout<<endl;
-    
-    string nm;
-    cout<<"ENTER NAME TO BE SEARCH: ";
-    cin>>nm;
-    for(int i=0;i<n;i++)
-    {
-        z[i]=p[i].name;
-    }
-    
-    if(binary_search(z, z+n, nm))
-    {
-        cout<<"****RECORD FOUND****";
-    }
-    else cout<<"****RECORD NOT FOUND****";
+
+    return 0;
 }
