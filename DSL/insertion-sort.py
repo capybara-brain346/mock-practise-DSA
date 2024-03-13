21e71f9d920921cbb174b2ee81ae80776f511f62
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-03-13 10:58:39 +0530

    insertion sort

--- DSL/insertion-sort.py
+++ DSL/insertion-sort.py
@@ -3,6 +3,6 @@ def insertion(list: list[int]) -> list[int]:
         j = i
         while list[j-1] > list[j] and j>0:
             list[j-1], list[j] = list[j],list[j-1]
-            j-=1
+            j-=    1
     return list
 print(insertion([4,3,1,8,6,0,9]))
